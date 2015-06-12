980a2bfaa6a991f80509c1b2489c1945d063ba6f
Ishay Frenkel <howtoevery.mail@gmail.com>
2015-06-12 07:47:29 +0300

    bug - app crashes

--- app/src/main/java/com/howtoevery/interactivestory/MainActivity.java
+++ app/src/main/java/com/howtoevery/interactivestory/MainActivity.java
@@ -1,38 +1,43 @@
 package com.howtoevery.interactivestory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.start_button);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                startStory(name);
+
+            }
+        });
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
